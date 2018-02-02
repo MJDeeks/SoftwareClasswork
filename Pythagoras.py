e0fcb17037adeb337f0836feed303c262bd8d10c
18dbrennan-rasmussen <18dbrennan-rasmussen@mlmc.vic.edu.au>
2018-02-02 11:44:41 +1100

    Completed c^2=a^2+b^2

--- Pythagoras.py
+++ Pythagoras.py
@@ -1,10 +1,11 @@
 import math
 
-a = input('please enter variable a: ')
-b = input('please enter variable b: ')
+a = int(input('please enter variable a: '))
+b = int(input('please enter variable b: '))
 
-c = math.sqrt(a**2+b**2) 
+c = math.sqrt(a**2+b**2)
+c = int(c)
 
-i = math.atan(a//b)
+i = math.atan(a/b)
 
-print('Your c variabale is: ')
+print('Your c variabale is: ',c)
